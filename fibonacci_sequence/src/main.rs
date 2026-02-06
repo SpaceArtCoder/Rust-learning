ec4dc4dbf38a7d7dde6b1b155d42447165b14cb9
Ilkham <valitovilkham@gmail.com>
2026-02-06 21:04:19 +0400

    fibonacci sequence

--- fibonacci_sequence/src/main.rs
+++ fibonacci_sequence/src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+
+    
+
+    fn fib(end : i32) {
+
+        let mut number = 1;
+
+        while number < end {
+        
+            println!("Value: {}", number);
+
+            number = number + 1;
+    
+        }
+    }
+
+    fib(8);
+    
 }
