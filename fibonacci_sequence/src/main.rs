7edc47925701b3deeb6a803b4adbe6c306414d90
Ilkham <valitovilkham@gmail.com>
2026-02-10 00:04:32 +0400

    solution v1

--- fibonacci_sequence/src/main.rs
+++ fibonacci_sequence/src/main.rs
@@ -4,17 +4,35 @@ fn main() {
 
     fn fib(end : i32) {
 
-        let mut number = 1;
+        let mut first_num = 0;
 
-        while number < end {
+        let mut second_num = 1;
+
+        let mut third_num;
+
+        let mut count = 0;
+
+        while count < end {
         
-            println!("Value: {}", number);
+            println!("Value: {}", first_num);
+            count+=1;
 
-            number = number + 1;
+            println!("Value: {}", second_num);
+            count+=1;
+
+            third_num = first_num + second_num;
+
+            first_num = third_num + second_num;
+
+            second_num = third_num + first_num;
+
+            println!("Value: {}", third_num);
+            count+=1;
+
     
         }
     }
 
-    fib(8);
+    fib(4);
     
 }
