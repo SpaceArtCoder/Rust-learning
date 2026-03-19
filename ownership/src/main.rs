bf96f7ebaa609ce6456bd6b04d66eed40b098a95
Ilkham <valitovilkham@gmail.com>
2026-03-19 21:32:29 +0400

    ownership

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -144,6 +144,8 @@ fn main() {                                     // Переменная еще �
     let mut s = String::from("hello");
 
     let r1 = &s;
+
+    let r2 = &s;
 }
 
 // fn change(some_string: &mut String) {                       // И приняли ее с помощью some_string: &mut String
