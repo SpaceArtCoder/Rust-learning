c75ef139fdcdbea828911f1ac4f4ecbb0920496c
Ilkham <valitovilkham@gmail.com>
2026-04-17 12:04:11 +0400

    structs

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -5,4 +5,11 @@ fn main() {
         sign_in_count: u64,                                     // скобок определяются имя и тип для элементов данных, они
         active: bool,                                           // именуются как "поля"
     }
+
+    let user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    }
 }
