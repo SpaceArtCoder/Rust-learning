4e36552bde7b68072b9f3f9b0ed0f00366753289
Ilkham <valitovilkham@gmail.com>
2026-05-08 21:09:48 +0400

    structs

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -90,7 +90,10 @@ fn main() {
 
     fn main() {
         let user1 = User {
-            
+            email: "someone@example.com",
+            username: "someusername123",
+            active: true,
+            sign_in_count: 1,
         }
     }
     
