f3049813ea246cd56896afeab98fba97023ae281
Ilkham <valitovilkham@gmail.com>
2026-05-13 21:43:53 +0400

    rectangles

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,4 +1,11 @@
 fn main() {
     let width1 = 30;
     let height1 = 50;
+
+    println!(
+        "Площадь проямоугольника равна {} квадратным пикселям.",
+        area(width1, height1)
+    );
 }
+
+
