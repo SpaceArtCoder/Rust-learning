02f1133d811cc245a1d7afadaf48271968ad24cd
Ilkham <valitovilkham@gmail.com>
2026-06-09 22:20:42 +0400

    rectangles

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -99,6 +99,12 @@ fn main() {
         height: u32,
     }
 
+    impl Rectangle {
+        fn area(&self) -> u32 {
+            
+        }
+    }
+
     let rect1 = Rectangle {width: 30, height: 50};
     let rect2 = Rectangle {width: 10, height: 40};
     let rect3 = Rectangle {width: 60, height: 45};
