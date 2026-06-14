6b2563ed368d28c699841299e47822ba8d273245
Ilkham <valitovilkham@gmail.com>
2026-06-14 21:56:56 +0400

    rectangles

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -151,4 +151,10 @@ fn main() {
         }
     }
 
+    impl Rectangle {
+        fn can_hold(&self, other: &Rectangle) -> bool {
+            self.width > other.width && self.height > other.height
+        }
+    }
+
 }
