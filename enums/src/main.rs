27df8e031a3a971077d8284e6840d6068a8664ef
Ilkham <valitovilkham@gmail.com>
2026-07-05 21:06:49 +0400

    enums

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -171,4 +171,13 @@ fn main() {
         Dime,                                                                             // чение с серией паттернов, а затем испольнять
         Quarter,                                                                          // код, основываясь на том, какой паттерн совпа-
     }                                                                                     // дает.
+
+    fn value_in_cents(coin: Coin) -> u8 {
+        match coin {
+            Coin::Penny => 1,
+            Coin::Nickel => 5,
+            Coin::Dime => 10,
+            Coin::Quarter => 25,
+        }
+    }
 }
