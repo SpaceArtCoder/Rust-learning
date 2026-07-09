236848b4217d8788bbc447383fd247616527f95c
Ilkham <valitovilkham@gmail.com>
2026-07-09 21:40:46 +0400

    enums

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -209,4 +209,16 @@ fn main() {
         Dime, 
         Quarter(UsState),
     }
+
+    fn value_in_cents(coin: Coin) -> u8 {
+        match coin {
+            Coin::Penny => 1,
+            Coin::Nickel => 5,
+            Coin::Dime => 10,
+            Coin::Quarter(state) => {
+                println!("Четвертак из штата {:?}!", state);
+                25
+            },
+        }
+    }
 }
