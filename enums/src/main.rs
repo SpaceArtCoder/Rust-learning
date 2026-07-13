1aa91dca2b09ab149e49db8fdbc87c064a9aace7
Ilkham <valitovilkham@gmail.com>
2026-07-13 19:51:33 +0400

    enums

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -236,7 +236,7 @@ fn main() {
     }                                                                                          // какие-либо операции.
 
     let five = Some(5);
-    let six = plus_one(five); 
+    let six = plus_one(five);
     let none = plus_one(None);
 
 }
