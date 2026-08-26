47ee8ddc486ffc13da27ae38ca86423cc39317ca
Ilkham <valitovilkham@gmail.com>
2026-08-26 19:51:48 +0400

    restaurant

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -30,8 +30,16 @@
 
 mod front_of_house {
     mod hosting {
-        fn add_to_waitlist () {}
+        fn add_to_waitlist() {}
     }
+}
+
+pub fn eat_at_restaurant() {
+    // Абсолютный путь
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // Относительный путь
+    front_of_house::hosting::add_to_waitlist();
 }
 
 
