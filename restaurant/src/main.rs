5d6decde01b08ab02697880125749b8bf9c6a582
Ilkham <valitovilkham@gmail.com>
2026-09-21 20:43:28 +0400

    restaurant

--- restaurant/src/main.rs
+++ restaurant/src/main.rs
@@ -57,5 +57,22 @@
     //     fn cook_order() {}
     // }
 
+// Обозначение структур и перечислений как публичных
+
+    mod back_of_house {
+        pub struct Breakfast {
+            pub  toast: String,
+            seasonal_fruit: String,
+        }
+
+        impl Breakfast {
+            pub fn summer(toast: &str) -> Breakfast {
+                Breakfast {
+                    toast: String::from(toast),
+                    seasonal_fruit: String::from("персики"),
+                }
+            }
+        }
+    }
 
     
