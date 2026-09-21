0c6535a9f0a7ae633df7b9c4c4d3c21cf7a24297
Ilkham <valitovilkham@gmail.com>
2026-09-21 20:32:58 +0400

    restaurant

--- restaurant/src/main.rs
+++ restaurant/src/main.rs
@@ -46,13 +46,16 @@
     // }
 
 
-    fn serve_order() {}
+    // fn serve_order() {}
 
-    mod back_of_house {
-        fn fix_incorrect_order() {
-            cook_order();
-            super::serve_order();
-        }
+    // mod back_of_house {
+    //     fn fix_incorrect_order() {
+    //         cook_order();
+    //         super::serve_order();
+    //     }
 
-        fn cook_order() {}
-    }
+    //     fn cook_order() {}
+    // }
+
+
+    
